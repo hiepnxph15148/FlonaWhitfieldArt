dbe1e908713b302b79075d9c8d15539b015d6362
hiepnxph <hiepnxph@gmail.com>
2022-09-16 14:45:26 +0700

    Add project files.

---
+++ FlonaWhitfieldArt/App_Start/RouteConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FlonaWhitfieldArt
+{
+    public class RouteConfig
+    {
+        public static void Header(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Header",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "SiteLayout", action = "RenderHeader", id = UrlParameter.Optional }
+            );
+        }
+        public static void Footer(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Footer",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "SiteLayout", action = "RenderFooter", id = UrlParameter.Optional }
+            );
+        }
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ FlonaWhitfieldArt/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace FlonaWhitfieldArt.Controllers
+{
+    public class HomeController : SurfaceController
+    {
+        // GET: Home
+        private const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/Home/";
+        public ActionResult RenderFeatured()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Featured.cshtml");
+        }
+        public ActionResult RenderServices()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Services.cshtml");
+        }
+        public ActionResult RenderBlog()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Blog.cshtml");
+        }
+        public ActionResult RenderClients()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Clients.cshtml");
+        }
+    }
+}
